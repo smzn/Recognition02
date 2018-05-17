c58c8024eeefb56441ef3c4168dacb614e0a3035
mizuno <mizuno@172.20.10.4>
2018-05-17 12:41:52 +0900

    20180517

---
+++ src/recognition02/Recognition02_main.java
@@ -0,0 +1,38 @@
+package recognition02;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+
+public class Recognition02_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		VisualRecognition service = new VisualRecognition("2018-03-19");
+		service.setApiKey("85da8b99eec051cb1c8beb70582a20915c2886ba");
+
+		InputStream imagesStream = null;
+		try {
+			imagesStream = new FileInputStream("img/fruitbowl.jpg");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
+		  .imagesFile(imagesStream)
+		  .imagesFilename("fruitbowl.jpg")
+		  .threshold((float) 0.6)
+		  .owners(Arrays.asList("IBM"))
+		  .build();
+		ClassifiedImages result = service.classify(classifyOptions).execute();
+		System.out.println(result);
+
+	}
+
+}
